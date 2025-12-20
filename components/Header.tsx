a13d96a88bb6dc3d5b1433c71a75bcb0c98f1a3a
Shoukai Huang <huangshoukai@yeah.net>
2025-12-20 23:03:53 +0800

    feat(assets): 更新网站图标集和默认背景图标
    refactor(Header): 替换Logo组件为ApImage组件显示网站图标

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import ApImage from '@/components/Image'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -17,7 +17,12 @@ const Header = () => {
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
           <div className="mr-3">
-            <Logo />
+            <ApImage
+              src="/static/favicons/default.svg"
+              alt="ApFramework Logo"
+              width={44}
+              height={44}
+            />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
